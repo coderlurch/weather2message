d64e4a8db66f41f8675c2a256cc570d294063f45
Marius Rimmler <marius@IKP028.ikp.kfa-juelich.de>
2021-09-12 17:49:21 +0200

    add even, odd week difference

--- main.py
+++ main.py
@@ -8,14 +8,23 @@ is_weekday_tmrw = tmrw.weekday() in range(5)
 
 if is_weekday_tmrw:
     for contact in contact_data.keys():
-        weather_message = f"Hallo {contact}! \n\n"
+        personal_motion_data = motion_profile[contact]
         try:
-            times = motion_profile[contact]["Weekday"]
-        except AttributeError:
-            times = motion_profile[contact][tmrw.weekday()]
+            if tmrw.date().isocalendar()[1]%2 == 0:
+                valid_motion_data = personal_motion_data["even"]
+            elif tmrw.date().isocalendar()[1]% 2 != 0:
+                valid_motion_data = personal_motion_data["odd"]
+        except KeyError:
+            valid_motion_data = personal_motion_data["general"]
+        weather_message = f"\n\nHallo {contact}! \n\n"
+        try:
+            times = valid_motion_data["Weekday"]
+        except KeyError:
+            times = valid_motion_data[tmrw.weekday()]
         for time in times.keys():
-            tmrw = tmrw.replace(hour=time, minute=0)
+            tmrw = tmrw.replace(hour=time-1, minute=0)
             location = times[time]
             weather_temp = weather.Weather(location, tmrw)
-            weather_message = weather_message + weather_temp.report() + "\n"
-        message.send_message(weather_message, contact_data[contact])
+            weather_message = weather_message + weather_temp.report()
+        print(weather_message)
+        #message.send_message(weather_message, contact_data[contact])
